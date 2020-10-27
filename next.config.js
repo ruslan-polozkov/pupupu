806deec2af70637aad8813f4ca41f8ebd3de880b
r.palazkou <r.palazkou>
2020-10-27 09:31:18 +0300

    123

--- next.config.js
+++ next.config.js
@@ -8,19 +8,6 @@ module.exports = withSass(withFonts({
 }));
 
 module.exports = withSass(withFonts({
-    // plugins: [
-    //     withSass(withFonts({
-    //         webpack(config, options) {
-    //             return config;
-    //         },
-    //     }))
-    // ],
-    // sassOptions: {
-    //     includePaths: [path.join(__dirname, 'styles')],
-    // },
-    // webpack(config, options) {
-    //     return config;
-    // },
     target: 'serverless',
     experimental: { scss: true },
     trailingSlash: true,
